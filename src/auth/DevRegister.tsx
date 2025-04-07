c737f14267c2e04f203c759421cc8dadab7c11ae
Priyanshu <priya14anshu@gmail.com>
2025-04-07 16:14:32 +0530

    Removed unused imports

--- src/auth/DevRegister.tsx
+++ src/auth/DevRegister.tsx
@@ -1,4 +1,4 @@
-import { useForm, SubmitHandler, set } from "react-hook-form"
+import { useForm, SubmitHandler} from "react-hook-form"
 import './auth.css'
 import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod"
