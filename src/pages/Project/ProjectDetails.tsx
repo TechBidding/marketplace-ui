b9a1fdf55dfbf60f31860d06716d10d628e78962
Priyanshu <priya14anshu@gmail.com>
2025-05-17 12:52:46 +0530

    Implement bid action handling in ProjectDetails component, allowing users to accept or reject bids with updated state management and UI feedback. Clean up console logs and enhance BidCard to conditionally display action buttons based on bid status.

--- src/pages/Project/ProjectDetails.tsx
+++ src/pages/Project/ProjectDetails.tsx
@@ -90,12 +90,6 @@ export default function ProjectDetails() {
     setIsBidPopupOpen(false);
   };
 
-  console.log(showBid, showMilestones)
-  console.log("All Bids: ", allBids);
-  console.log("all milestones: ", allMilestones);
-
-  console.log(userDetails)
-
 
   useEffect(() => {
     setLoading(true);
@@ -164,6 +158,30 @@ export default function ProjectDetails() {
       })
     }
   }, [showBid, showMilestones, activeTab, project])
+
+  const params = useParams()
+  const handleBidAction = (bidId: number, action: string) => {
+    let url = `project/${params.id}/bid/${bidId}/${action}`
+
+    projectHttp.put(url).then((res) => {
+      toast.success(`Bid ${action}ed`);
+      const updatedBid = res.data;
+      if (action === 'accept') {
+        let filteredBids = allBids.filter((bid) => bid._id === bidId);
+        let newAllBids = [...filteredBids, updatedBid];
+        setAllBids(newAllBids)
+      }
+      else if(action === 'reject') {
+        let filteredBids = allBids.filter((bid) => bid._id === bidId);
+        setAllBids(filteredBids)
+      }
+    })
+      .catch((err) => {
+        toast.error(`Bid ${action} unsuccessfull`, {
+          description: err.response.data.message
+        })
+      })
+  }
 
 
 
@@ -305,7 +323,7 @@ export default function ProjectDetails() {
               <div className="mt-8 space-y-4">
                 {allBids && allBids.map((bid) => {
                   return (
-                    <BidCard bid={bid} hoverClr={hoverClr} borderClr={borderClr} expanded={expanded} setExpanded={setExpanded} />
+                    <BidCard bid={bid} hoverClr={hoverClr} borderClr={borderClr} expanded={expanded} setExpanded={setExpanded} handleBidAction={handleBidAction} />
                   )
                 })}
               </div>
@@ -401,13 +419,22 @@ interface BidProps {
   borderClr: string;
   expanded: Record<number, boolean>;
   setExpanded: (expanded: Record<number, boolean>) => void;
+  handleBidAction: (bidId:string, action:'accept' | 'reject')=>void
 }
-const BidCard: React.FC<BidProps> = ({ bid, hoverClr, borderClr, expanded, setExpanded }) => {
+const BidCard: React.FC<BidProps> = ({ bid, hoverClr, borderClr, expanded, setExpanded, handleBidAction }) => {
 
-  console.log("Bid: ", bid)
-  const handleBidAction = (bidId: number, action: string) => {
-    console.log(bidId, action);
+  let showAccept = true;
+  let showReject = true;
+  let showBidAction = !showAccept && !showReject
+
+  if (bid.status === 'accepted') {
+    showAccept = false;
+    showReject = false;
   }
+  if (bid.status === 'rejected') {
+    showReject = false;
+  }
+
 
   return (
     <div
@@ -417,7 +444,7 @@ const BidCard: React.FC<BidProps> = ({ bid, hoverClr, borderClr, expanded, setEx
       <div className={`flex items-center justify-between w-full gap-5 ${expanded ? "mb-4" : ""}`}>
         <div className="flex items-center justify-between w-full cursor-pointer" onClick={() => setExpanded({ [bid._id]: !expanded[bid._id] })}>
           <div className="flex items-center gap-3">
-            {bid.status === "Accepted" ? (
+            {bid.status === "accepted" ? (
               <MdPlaylistAddCheck className="h-5 w-5 text-emerald-600" />
             ) : (
               <MdOutlinePlaylistAdd className="h-5 w-5 text-gray-400" />
@@ -425,12 +452,12 @@ const BidCard: React.FC<BidProps> = ({ bid, hoverClr, borderClr, expanded, setEx
             <span className="font-medium text-lg">{bid.description}</span>
           </div>
           <div className="flex items-center gap-2">
-            {bid.status === "Pending" && <span className="text-sm text-yellow-600 font-semibold">Pending</span>}
-            {bid.status === "Rejected" && <span className="text-sm text-red-600 font-semibold">Rejected</span>}
-            {bid.status === "Accepted" && <span className="text-sm text-green-600 font-semibold">Accepted</span>}
+            {bid.status === "pending" && <span className="text-sm text-yellow-600 font-semibold">Pending</span>}
+            {bid.status === "rejected" && <span className="text-sm text-red-600 font-semibold">Rejected</span>}
+            {bid.status === "accepted" && <span className="text-sm text-green-600 font-semibold">Accepted</span>}
           </div>
         </div>
-        <div className="flex items-center gap-4">
+        <div className={`flex items-center gap-4 ${showBidAction ? 'show' : 'hidden'}`}>
           <button className="text-sm text-gray-400 hover:text-gray-600 cursor-pointer">
             <DropdownMenu>
               <DropdownMenuTrigger>
@@ -439,8 +466,13 @@ const BidCard: React.FC<BidProps> = ({ bid, hoverClr, borderClr, expanded, setEx
               <DropdownMenuContent>
                 <DropdownMenuLabel>Bid Actions</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => handleBidAction(bid._id, "accept")} className="text-green-600 cursor-pointer">Accept</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleBidAction(bid._id, "reject")} className="text-red-600 cursor-pointer">Reject</DropdownMenuItem>
+                {showAccept && (
+                  <DropdownMenuItem onClick={() => handleBidAction(bid._id, "accept")} className="text-green-600 cursor-pointer">Accept</DropdownMenuItem>
+                )}
+                {showReject && (
+                  <DropdownMenuItem onClick={() => handleBidAction(bid._id, "reject")} className="text-red-600 cursor-pointer">Reject</DropdownMenuItem>
+                )}
+                
               </DropdownMenuContent>
             </DropdownMenu>
 
